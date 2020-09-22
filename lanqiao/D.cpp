07dfbbe4cc5bc6956cfd4eb3862c1f8204c8bb76
Naokisato <821874555@qq.com>
2020-09-22 11:39:20 +0800

    1231

--- lanqiao/D.cpp
+++ lanqiao/D.cpp
@@ -15,24 +15,55 @@ typedef unsigned long long ull;
 const int INF = 1e9;
 typedef pair<int,int> pii;
 
+queue<int> q[5];
+void init(int n)
+{
+    for (int i = n; i <= 4;i++) {
+        while(!q[i].empty())
+            q[i].pop();
+        q[i].push(1);
+    }
+}
+
 
 int main()
 {
-    int t = 1000;
+    int n = 1000;
+    for (int i = 0; i <= 4;i++) {
+        q[i].push(1);
+    }
+    for (int i = 1; i <= 4;i++) {
+        q[i].push(1);
+    }
+    ll ans = 0;
 
-
-    while(t--)
-    {
+    while (n--){
         string s;
         char c;
-        c = getchar();
-        while (c != '\n') {
-            if(c!=' '&&c!='='&&c!='+')
-                s.push_back(c);
+        c=getchar();
+        while(c!='\n'){
+            s.push_back(c);
+            c = getchar();
         }
-
-        if(s[0]=='R'){
-            
+        cout << s << endl;
+        int zero = 0;
+        for (int i = 0; i < s.size();i++){
+            if(s[i]!=' '){
+                if(s[i]=='R'){
+                    init(zero / 4 + 1);
+                    int temp = q[zero / 4].front() * (s[s.size() - 2]-'0');
+                    q[zero / 4 + 1].pop();
+                    q[zero / 4 + 1].push(temp);
+                }
+                else{
+                    ans += (s.back() - '0') * q[zero / 4].front();
+                }
+                break;
+            }   
+            else
+                zero++;
         }
+        cout << ans << endl;
     }
+    
 }
--- test.cpp
+++ test.cpp
@@ -1,43 +1,24 @@
+#include <iostream>
+#include <cstring>
 #include <algorithm>
+#include <queue>
+#include <map>
+#include <unordered_map>
 #include <cmath>
 #include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 typedef long long ll;
 typedef unsigned long long ull;
-const int INF = -1e9 - 50;
-typedef pair<int, int> pii;
+const int INF = 1e9;
+typedef pair<int,int> pii;
 
-const int N = 1050;
-int n;
-int a[N];
-int dp[N];
 
 int main()
 {
-    cin >> n;
-
-    for (int i = 1; i <= n; i++) {
-        cin >> a[i];
-    }
-    a[0] = -1e9 - 50;
-    for (int i = 1; i <= n; i++) { //子序列最后一个数字pos为i的
-        for (int j = 0; j < i; j++) {
-            if (a[j] < a[i]) {
-                dp[i] = max(dp[i], dp[j] + 1);
-            }
-        }
-    }
-    int ans = 0;
-    for (int i = 1; i <= n; i++) {
-        ans = max(ans, dp[i]);
-    }
-    cout << ans << endl;
+    char c;
+    c.getchar();
+    while(c!='\n')
 }

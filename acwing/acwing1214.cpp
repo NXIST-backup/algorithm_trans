9fbf5bf1580403ed80f97efff1295580d2703326
Naokisato <821874555@qq.com>
2020-09-25 13:20:32 +0800

    qwe

--- acwing/acwing1214.cpp
+++ acwing/acwing1214.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-#include <cstring>
 #include <algorithm>
-#include <queue>
-#include <map>
-#include <unordered_map>
 #include <cmath>
 #include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -13,30 +13,28 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 const int INF = 1e9;
-const int MOD = 1e9 + 7;
+const int MOD = 100000007;
 typedef pair<int, int> pii;
 
 const int N = 1e3 + 50;
 int n, s, a, b;
 int dp[N][N];
-int sum[N][N];
+
+int get_mod(int a, int b)
+{
+    return (a % b + b) % b;
+}
+
 int main()
 {
     cin >> n >> s >> a >> b;
 
-    for (int j = 0;j<=n;j++) {
-        dp[0][j] = 1;
-    }
-    for (int i = 1; i <= n; i++) {//a的个数
-        for (int j = 1;j<=n;j++){//长度
-
-            dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1]&MOD;
-            
+    dp[0][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n; j++) {
+            dp[i][j] = (dp[i - 1][get_mod(j - a * (n - i), n)] + dp[i - 1][get_mod(j + b * (n - i), n)]) % MOD;
         }
     }
-    ll ans = 0;
-    for (int i = 0; i <= n; i++) {
-        ans += dp[i][n] % MOD;
-    }
-    cout << ans << endl;
+
+    cout << dp[n - 1][get_mod(s, n)] << endl;
 }
---
+++ template/BasicalAlgorithm/get_mod.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+const int INF = 1e9;
+typedef pair<int,int> pii;
+
+
+int get_mod(int a,int b)
+{
+    return (a % b + b) % b;
+}

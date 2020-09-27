ee7c70ee5ef29f9b88d4007aa7ea9afbd455aca8
Naokisato <821874555@qq.com>
2020-09-27 12:20:09 +0800

    123

--- acwing/acwing1210.cpp
+++ acwing/acwing1210.cpp
@@ -15,3 +15,31 @@ typedef unsigned long long ull;
 const int INF = 1e9;
 typedef pair<int,int> pii;
 
+int n;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << fixed;
+
+    cin >> n;
+
+    vector<int> a(n);
+
+    for (int i = 0;i<n;i++)
+        cin >> a[i];
+    ll res = 0;
+    for (int i = 0; i < n; i++) {
+        int mx = -1e9, mn = +1e9;
+
+        for(int j=i;j<n;j++){
+            mx = max(mx, a[j]);
+            mn = min(mn, a[j]);
+            if(mx-mn==j-i)
+                res++;
+        }
+    }
+
+    cout << res << endl;
+}
---
+++ acwing/acwing1236.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+const int INF = 1e9;
+typedef pair<int, int> pii;
+
+const int N = 1e5 + 50;
+
+int n;
+int a[N], b[N], c[N];
+int main()
+{
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+    for (int i = 0; i < n; i++)
+        cin >> c[i];
+
+    sort(a, a + n);
+    sort(b, b + n);
+    sort(c, c + n);
+
+
+}

dca1a627501dfaa078ee2d85aa8b17e4187d97d0
Naokisato <821874555@qq.com>
2021-01-12 00:00:06 +0800

    23324

--- atcoder/abc188E.cpp
+++ atcoder/abc188E.cpp
@@ -19,3 +19,30 @@ typedef long long ll;
 typedef unsigned long long ull;
 const int mod = 1e9 + 7;
 typedef pair<int, int> pii;
+
+const int N = 2e5 + 5;
+int n, m;
+vector<int> g[N];
+int w[N];
+int dp[N];
+int main()
+{
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        cin >> w[i];
+    }
+    for (int i = 1; i <= m; i++) {
+        int a, b;
+        cin >> a >> b;
+        g[b].push_back(a);
+    }
+    int ans = -mod;
+    for (int i = 1; i <= n; i++) {
+        dp[i] = w[i];
+        for (auto v : g[i]) {
+            ans = max(ans, w[i] - dp[v]);
+            dp[i] = min(dp[i], dp[v]);
+        }
+    }
+    cout << ans << endl;
+}

246654c8949cbd9e682967e24b9648eb482bb386
Naokisato <821874555@qq.com>
2021-01-27 16:58:54 +0800

    123

---
+++ algorithm_improve/DP/queue_improve/acwing1087.cpp
@@ -0,0 +1,23 @@
+/*
+  Problem Name:
+  algorithm tag:
+*/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+const int INF = 1e9;
+const int mod = 1e9 + 7;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;

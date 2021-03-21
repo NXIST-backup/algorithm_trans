82f43f6ffefbbcd6ebce2259a2758877e7abe0e2
Naoki’s ipad <821874555@qq.com>
2021-03-21 22:31:24 +0800

    123

---
+++ algorithm_improve/Search/acwing188.cpp
@@ -0,0 +1,33 @@
+/*
+  Problem Name:武士风度的牛
+  algorithm tag:Flood fill,BFS
+*/
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+const int INF = 1e9;
+const int mod = 1e9 + 7;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define iosf ios::sync_with_stdio(false), cin.tie(0), cout << fixed
+
+char g[1]
+int n,m;
+
+int main()
+{
+
+}
+

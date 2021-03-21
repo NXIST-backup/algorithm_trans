5786771443b49c4ef9cfb11a4f947e232b3dfcb8
Naoki’s ipad <821874555@qq.com>
2021-03-21 23:55:41 +0800

    123

--- algorithm_improve/Search/acwing188.cpp
+++ algorithm_improve/Search/acwing188.cpp
@@ -23,11 +23,27 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 #define iosf ios::sync_with_stdio(false), cin.tie(0), cout << fixed
 
-char g[1]
+char g[155][155];
 int n,m;
+int dx[8]={1,1,-1,-1,2,2,-2,-2};
+int dy[8]={2,-2,2,-2,1,-1,1,-1};
+int dz[8]={1,-1,1,-1,};
 
 int main()
 {
+    iosf;
+
+    cin >> n >> m;
+    int x1,y1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            char c;
+            cin>>c;
+            g[i][j]=c;
+            if(c=='K')
+                x1=i,y1=j;
+        }
+    }
 
 }
 

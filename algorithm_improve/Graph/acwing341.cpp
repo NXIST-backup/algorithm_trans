6811b5997c3e8772ee743a1bc242c7001b42fd85
Naokisato <821874555@qq.com>
2021-04-06 13:31:56 +0800

    123234

--- algorithm_improve/Graph/acwing341.cpp
+++ algorithm_improve/Graph/acwing341.cpp
@@ -29,69 +29,46 @@ typedef pair<ll, ll> pll;
 //dfs 记忆化搜索
 //spfa
 
-const int N = 1e5 + 5,M = 1e6 + 5;
-int h[N*3],e[M*3],ne[M*3],w[M*3],idx;
+const int N = 1e5 + 5, M = 1e6 + 5;
+int h[N], hb[N], e[M], ne[M], idx;
 int val[N];
-int dist[N*3];
-int st[N*3];
-int q[N*3];
-int n,m;
-void add(int a,int b,int c)
+int n, m;
+bool is_valid[N];
+
+void add(int h[], int a, int b)
 {
-  e[idx]=b;
-  ne[idx]=h[a];
-  w[idx]=c;
-  h[a]=idx++;
+    e[idx] = b;
+    ne[idx] = h[a];
+    h[a] = idx++;
 }
-void addEdge(int a,int b)
+void dfs(int u)
 {
-  add(a,b,0);
-  add(a+n,b+n,0);
-  add(a+2*n,b+2*n,0);
-  add(a,a+n,-val[a]);
-  add(a,a+2*n,val[a]);
-}
-void spfa()
-{
-  int hh=0,tt=0;
-  memset(dist,-0x3f,sizeof dist);
-  dist[1]=0;
-  st[1]=1;
-  q[0]=1;
-  while(hh>=tt){
-    int t = q[hh++];
+    is_valid[u] = 1;
 
-    st[t]=0;
-
-    for(int i=h[t];~i;i=ne[i]){
-      int j = e[i];
-
-      if(dist[j]<dist[t]+w[i]){
-        dist[j]=dist[t]+w[i];
-        st[j]=1;
-        q[++tt]=j;
-      }
+    for (int i = hb[u]; ~i; i = ne[i]) {
+        int j = e[i];
+        if (!is_valid[j])
+            dfs(j);
     }
-  }
 }
+
 int main()
 {
-  iosf;
-  memset(h,-1,sizeof h);
-  cin >> n >> m;
+    iosf;
+    cin >> n >> m;
 
-  for(int i = 1;i <= n;i++)
-    cin>>w[i];
+    for (int i = 1; i <= n; i++)
+        cin >> val[i];
 
-  for(int i=1;i<=m;i++){
-    int a,b,c;
-    cin>>a>>b>>c;
-    addEdge(a,b);
-    if(c==2)
-      addEdge(b,a);
-  }
+    for (int i = 1; i <= m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        add(h, a, b);
+        add(hb, b, a);
+        if (c == 2)
+            add(h, b, a), add(hb, a, b);
+    }
 
-  spfa();
+    dfs(n);
 
-  cout<<dist[3*n]<<endl;
 }

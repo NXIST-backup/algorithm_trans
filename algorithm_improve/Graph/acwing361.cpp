c3087ba20c1de1d6a0ab58a538a6e628eaad05b2
Naokisato <821874555@qq.com>
2021-04-13 16:27:27 +0800

    123

--- algorithm_improve/Graph/acwing361.cpp
+++ algorithm_improve/Graph/acwing361.cpp
@@ -19,6 +19,7 @@ typedef long long ll;
 typedef unsigned long long ull;
 const int INF = 1e9;
 const int mod = 1e9 + 7;
+const double eps = 1e-4;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 //#define x first
@@ -27,10 +28,10 @@ typedef pair<ll, ll> pll;
 
 const int N = 1005, M = 5005;
 int n, m;
-int h[N], e[M], ne[M], idx; 
-double w[M],cy[M];
-int f[N];
-int dist[N];
+int h[N], e[M], ne[M], idx;
+double w[M], cy[M];
+double f[N];
+double dist[N];
 int st[N], cnt[M];
 
 void add(int a, int b, double c)
@@ -42,58 +43,60 @@ void add(int a, int b, double c)
 }
 bool spfa()
 {
-    memset(dist,0,sizeof dist);
-    memset(st,0,sizeof st);
-    memset(cnt,0,sizeof cnt);
+    // 注意求最小环判负是不行的
+    memset(dist, 0, sizeof dist);
+    memset(st, 0, sizeof st);
+    memset(cnt, 0, sizeof cnt);
     queue<int> q;
-    for(int i=1;i<=n;i++)
-        q.push(i),st[i]=true;
-    
-    while(q.size()){
+    for (int i = 1; i <= n; i++)
+        q.push(i), st[i] = true;
+
+    while (q.size()) {
         int u = q.front();
         q.pop();
 
         st[u] = false;
 
-        for(int i=h[u];~i;i=ne[i]){
+        for (int i = h[u]; ~i; i = ne[i]) {
             int v = e[i];
-            if(dist[v]>dist[u]+w[i]){
-                dist[v]=dist[u]+w[i];
+            if (dist[v] < dist[u] + w[i]) {
+                dist[v] = dist[u] + w[i];
                 cnt[i]++;
-                if(cnt>=n)
+                if (cnt[i] >= n)
                     return true;
-                if(!st[v]){
+                if (!st[v]) {
                     q.push(v);
-                    st[v]=true;
+                    st[v] = true;
                 }
             }
         }
     }
     return false;
-    
 }
 int main()
 {
     iosf;
     cin >> n >> m;
-
+    memset(h, -1, sizeof h);
     for (int i = 1; i <= n; i++)
         cin >> f[i];
     for (int i = 1; i <= m; i++) {
         int a, b, c;
-        add(a, b, -c);
+        cin >> a >> b >> c;
+        add(a, b, c);
     }
 
-    double l = 0, r = 1000;
-    while (l > r) {
-        double mid = l + r >> 1;
+    double l = 0, r = 1e6;
+    while (r - l > eps) {
+        double mid = (l + r) / 2;
         memcpy(cy, w, sizeof cy);
         for (int i = 0; i < idx; i++)
-            w[i] = w[i] * mid + f[e[i]];
-
+            w[i] = f[e[i]] - w[i] * mid;
         if (spfa())
-            r = mid;
+            l = mid;
         else
-            l = mid + 1;
+            r = mid;
+        memcpy(w, cy, sizeof w);
     }
+    printf("%.2lf", l);
 }

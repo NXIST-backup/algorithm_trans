0a64871708cafb3ea8f849d15d20ae261ae2daa3
Naokisato <821874555@qq.com>
2021-05-17 18:23:33 +0800

    123123

--- template/Dynamic_Programing/bag_complete.cpp
+++ template/Dynamic_Programing/bag_complete.cpp
@@ -19,7 +19,6 @@
 #include <unordered_map>
 #include <vector>
 
-
 using namespace std;
 
 const int N = 1010;
@@ -28,15 +27,16 @@ int n, m;
 int v[N], w[N];
 int f[N];
 
-int main() {
-  cin >> n >> m;
+int main()
+{
+    cin >> n >> m;
 
-  for (int i = 1; i <= n; i++)
-    cin >> v[i] >> w[i];
+    for (int i = 1; i <= n; i++)
+        cin >> v[i] >> w[i];
 
-  for (int i = 1; i <= n; i++)
-    for (int j = v[i]; j <= m; j++)
-      f[j] = max(f[j], f[j - v[i]] + w[i]);
+    for (int i = 1; i <= n; i++)
+        for (int j = v[i]; j <= m; j++)
+            f[j] = max(f[j], f[j - v[i]] + w[i]);
 
-  cout << f[m] << endl;
+    cout << f[m] << endl;
 }
